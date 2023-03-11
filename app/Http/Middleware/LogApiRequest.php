4f49414d9101276676c88445e3db12ffeb6d118a
Daniel Burgos <danielfburgos9806@gmail.com>
2023-03-11 15:42:49 -0500

    Add log to apis requests

---
+++ app/Http/Middleware/LogApiRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogApiRequest
+{
+    /**
+     * Registra en el log la petición que se está solicitando
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info($request);
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function() {
+Route::prefix('v1')->middleware(['log.api'])->group(function() {
     Route::get('/users/{id}', [UserController::class, 'show']);
 });
 
