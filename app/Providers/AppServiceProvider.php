22732ae48c415c35a55fea3dbc47305d72905806
Daniel Burgos <danielfburgos9806@gmail.com>
2023-03-11 12:37:02 -0500

    Fix the base path of collection paginate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,13 +27,13 @@ class AppServiceProvider extends ServiceProvider
         if (!Collection::hasMacro('paginate')) {
             Collection::macro('paginate', function ($perPage = 15, $page = null, $options = []) {
                 $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
-                return new LengthAwarePaginator(
+                return (new LengthAwarePaginator(
                     $this->forPage($page, $perPage),
                     $this->count(),
                     $perPage,
                     $page,
                     $options
-                );
+                ))->withPath('');
             });
         }
     }
